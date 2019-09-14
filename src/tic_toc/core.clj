d71ad9bc49e1118cd8465c94095239df5330faa6
sfertman <sfertman@zoho.com>
2019-09-14 12:14:54 -0400

    better macros

--- src/tic_toc/core.clj
+++ src/tic_toc/core.clj
@@ -1,4 +1,6 @@
-(ns tic-toc.core)
+(ns tic-toc.core
+  (:require
+    [clojure.pprint :refer [pprint]]))
 
 ;;; timers
 (defn- now [] (System/nanoTime))
@@ -17,8 +19,8 @@
 
 (defn clear!
   ([] (clear! :default-timer))
-  ([timer] (swap! dissoc timers timer)))
-  
+  ([timer] (swap! timers dissoc timer)))
+
 ;;; metrics
 (defonce metrics (atom []))
 
@@ -29,60 +31,34 @@
   [timer]
   (toc
     timer
-    (fn [t T]
+    (fn [_ t]
       (let [dt (toc t)]
         (clear! t)
         (collect!* t dt)
         dt))))
-        
-        
+
+(defmacro fn-name [f] `(-> ~f resolve symbol keyword)) ;; <-- fn-name should prolly be a function
+
 (defn wrap-tictoc
   [form]
-  `(let [fn-key# (-> form first fn-name)]
+  `(let [fn-key# (-> '~form first fn-name)]
     (tic! fn-key#)
     (let [ret# ~form]
       (toc! fn-key#)
       ret#)))
-      
+
 (defmacro prof-1 [form] (wrap-tictoc form))
 
-(defmacro prof-n [forms] `(do ~@(map wrap-tictoc forms)))
-
-(comment """
-Could atually collect array of maps; sort of a column oriented db like cassandra
-each 'log' entry map may consist of
-  - fn-name  (mebbe even inputs and outputs?),
-  - caller
-  - execution time
-
-""")
+(defmacro prof-n [& forms] `(do ~@(map wrap-tictoc forms)))
 
 (def mx1 (comp pprint macroexpand-1))
 
-(defmacro fn-name [f] `(-> ~f resolve symbol keyword)) ;; <-- fn-name should prolly be a function
 ;; this macro must return something to indicate that f is not something that can be invoked
 ;; nil perhaps? -- TODO later
 
-(defmacro prof-1
-  [form]
-  `(let [fn-key# (fn-name '~(first form))] ;; <-- this works both with macro and fn
-    (tic! fn-key#)
-    (let [ret# ~form]
-      (collect! {:fn-name fn-key# :time-ns (toc fn-key#)})
-      ret#)))
 
 (prof-1 (byte-array [1 2 3 4]))
 
 
-(defmacro prof-n [& forms] 
-  `(do 
-    ~@(map (fn [form#] 
-            (let [fn-key# (-> form# first fn-name)]
-              (tic! fn-key#)
-              (let [ret# form#]
-                (collect! {:fn-name fn-key# :time-ns (toc fn-key#)})
-                ret#)) )
-            forms)))
-;; ^ THIS works for profiling a bunch of forms
 
 (prof-n (+ 2 3) (- 7 6) (str "42" "abc"))
