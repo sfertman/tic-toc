5de856d1094246a184fa6134da044d9362e9e593
sfertman <sfertman@zoho.com>
2019-10-17 01:15:26 -0400

    corrent memoize at the cost of a complicated fn

--- src/tic_toc/metrics.clj
+++ src/tic_toc/metrics.clj
@@ -15,46 +15,35 @@
   [metrics]
   (reduce upsert {} (map-indexed (fn [i metric] [(:fn-id metric) i] ) metrics)))
 
-(defn- args-time*
-  "Calculates argument time for a given metric"
-  [metrics index metric]
-  ;; ^^ may be problematic with recursion; possibly need to track; unique fn-id might save us here; test this to find out!
-  (if-let [arg-fns (-> metric :meta :arg-fns)]
-    (let [xform (comp (mapcat #(get index %))
-                      (map #(nth metrics %))
-                      (map :time-ns))]
-      (transduce xform + arg-fns))
-    0))
-(def args-time (memoize args-time*))
-
 (defn matcher [s] (re-matches #":(.*)__\d+$" s))
 (defn get-fn-name [fn-id] (-> fn-id str matcher second))
 
-(defn add-stat
-  [metrics index X x]
-  (let [total-time (+ (or (:total-time X) 0) (:time-ns x))
-        args-time* (args-time metrics index x)
-        fn-time (- total-time args-time*)
-        calls (+ (or (:calls X) 0) 1)]
-    {:total-time total-time
-     :args-time args-time*
-     :fn-time fn-time
-     :calls calls
-     :mean-total (double (/ total-time calls))
-     :mean-args (double (/ args-time* calls))
-     :mean-fn (double (/ fn-time calls))}))
-
-(defn summarizer
-  [metrics index summary metric]
-  (let [add-stat* (partial add-stat metrics index)]
-    (update
-      summary
-      (-> metric :fn-id get-fn-name)
-      add-stat*
-      metric)))
-
-(defn- summary* [metrics]
-  (let [summarizer* (partial summarizer metrics (index-metrics metrics))]
-    (reduce summarizer* {} metrics)))
-
-(def summary (memoize summary*))
+(defn summary
+  [metrics]
+  (let [index (index-metrics metrics)
+        arg-fn-time (memoize
+                      (fn [fn-name] ;; TODO: later should be done for fn-id if we're tracking line numbers
+                        (->>
+                          fn-name
+                          (get index)
+                          (map #(nth metrics %))
+                          (map :time-ns)
+                          (reduce +))))
+        args-time (fn [metric]
+                    (if-let [arg-fns (-> metric :meta :arg-fns seq)]
+                      (reduce + (map arg-fn-time arg-fns))
+                      0))
+        add-stat (fn [old-val x]
+                    (let [total-time (+ (or (:total-time old-val) 0) (:time-ns x))
+                          args-time* (args-time x)
+                          fn-time (- total-time args-time*)
+                          calls (+ (or (:calls old-val) 0) 1)]
+                      {:total-time total-time
+                      :args-time args-time*
+                      :fn-time fn-time
+                      :calls calls
+                      :mean-total (double (/ total-time calls))
+                      :mean-args (double (/ args-time* calls))
+                      :mean-fn (double (/ fn-time calls))}))
+        summarizer (fn [s m] (update s (-> m :fn-id get-fn-name) add-stat m))]
+    (reduce summarizer {} metrics)))
